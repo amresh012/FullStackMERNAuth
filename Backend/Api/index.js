04d8a143ee5843254ae69210961fc06fc809f862
amresh012 <73485115+amresh012@users.noreply.github.com>
2023-12-10 11:46:31 +0530

    connected to Mongodb

--- Backend/Api/index.js
+++ Backend/Api/index.js
@@ -1,6 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+// process.env.MONGOU
+
+mongoose.connect("mongodb+srv://amreshmishra67088:fHLqr906FXennbfX@mern.yijis4x.mongodb.net/mern-auth?retryWrites=true&w=majority")
+.then(()=>{console.log("Connected to MongoDB")})
+.catch((err)=>{console.log(err)})
 
 const app = express()
 
 
 app.listen(3000, ()=>{console.log(`Server is listning on port 3000! `)})
+
+
+
+// P--- fHLqr906FXennbfX
